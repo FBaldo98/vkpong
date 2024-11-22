7a2f9edebe70ffe007b6e4bdba1d8f9a786e1b63
FBaldo98 <francesco.baldisserri98@outlook.com>
2024-11-22 14:19:47 +0100

    Swapchain and image views creation

--- be_renderer.cpp
+++ be_renderer.cpp
@@ -270,7 +270,30 @@ namespace be {
 
 		void BeRenderer::createImageViews()
 		{
-			
+			swapChainImageViews.resize(swapChainImages.size());
+
+			for (size_t i = 0; i < swapChainImages.size(); i++)
+			{
+				VkImageViewCreateInfo createInfo = {};
+				createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+				createInfo.image = swapChainImages[i];
+				createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+				createInfo.format = swapChainImageFormat;
+				
+				createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+				createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+				createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+				createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+				createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+				createInfo.subresourceRange.baseMipLevel = 0;
+				createInfo.subresourceRange.levelCount = 1;
+				createInfo.subresourceRange.baseArrayLayer = 0;
+				createInfo.subresourceRange.layerCount = 1;
+
+				if (vkCreateImageView(vkDevice, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS)
+					throw std::runtime_error("Failed to create image view");
+			}
 		}
 
 		VkPresentModeKHR BeRenderer::chooseSwapPresentMode(const ::std::vector<VkPresentModeKHR>& availablePresentModes)
@@ -396,6 +419,9 @@ namespace be {
 
 		void BeRenderer::terminateVk()
 		{
+			for (auto iv : swapChainImageViews)
+				vkDestroyImageView(vkDevice, iv, nullptr);
+
 			vkDestroySwapchainKHR(vkDevice, swapChain, nullptr);
 			vkDestroyDevice(vkDevice, nullptr);
 			vkDestroySurfaceKHR(vkInstance, vkSurface, nullptr);
