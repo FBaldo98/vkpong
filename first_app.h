f9c3bd266afdf3b70a6aa4e8cf05997c8b337fc9
FBaldo98 <francesco.baldisserri98@outlook.com>
2024-11-22 09:04:44 +0100

    Vulkan instance initialization code

--- first_app.h
+++ first_app.h
@@ -2,6 +2,11 @@
 
 #include "be_window.h"
 
+#define VK_USE_PLATFORM_WIN32_KHR
+#include <vulkan/vulkan.h>
+
+#include <vector>
+
 namespace be 
 {
 	
@@ -13,12 +18,34 @@ namespace be
 
 		FirstApp() {
 			initWindow(window, WIDTH, HEIGHT, "Hello World");
+
+			initVk();
 		}
 
 		void run();
 
 	private:
+		bool initVk();
+		bool getVkPhysicalDevice();
+		void terminateVk();
+
+		std::vector<const char*> getRequiredExtensions();
+		bool checkValidationLayerSupport();
+
+	private:
 		BeWindow window = {};
+
+		VkInstance vkInstance;
+
+		const std::vector<const char*> validationLayers = {
+			"VK_LAYER_KHRONOS_validation"
+		};
+
+#ifdef _DEBUG
+		const bool enableValidationLayers = true;
+#else
+		const bool enableValidationLayers = false;
+#endif
 	};
 
 }
